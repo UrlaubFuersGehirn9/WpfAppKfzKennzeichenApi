e048c95017961f07e582f3c564dfeb07e9b86f90
UrlaubFuersGehirn9 <luca.kaufmann1998@gmail.com>
2024-11-22 11:33:07 +0100

    create works, put+del not

---
+++ WpfAppKfzKennzeichenApi/KennzeichenInfos.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppKfzKennzeichenApi
+{
+    internal class KennzeichenInfos
+    {
+
+
+        public Kennzeichen[] Property1 { get; set; }
+    }
+
+    public class Kennzeichen
+    {
+        public int id { get; set; }
+        public string kennzeichen { get; set; }
+        public string ort { get; set; }
+        public string kreis { get; set; }
+        public string bundesland { get; set; }
+        public string wappen { get; set; }
+        public override string ToString()
+        {
+            string str= id+ " " + kennzeichen + " " + ort;
+            return str;
+        }
+
+        public Kennzeichen(int id, string kennzeichen, string ort, string kreis, string bundesland, string wappen)
+        {
+            this.id = id;
+            this.kennzeichen = kennzeichen;
+            this.ort = ort;
+            this.kreis = kreis;
+            this.bundesland = bundesland;
+            this.wappen = wappen;
+        }
+
+        public Kennzeichen(string kennzeichen, string ort, string kreis, string bundesland, string wappen)
+        {
+            this.kennzeichen = kennzeichen;
+            this.ort = ort;
+            this.kreis = kreis;
+            this.bundesland = bundesland;
+            this.wappen = wappen;
+        }
+
+        public Kennzeichen()
+        {
+        }
+    }
+}
--- WpfAppKfzKennzeichenApi/MainWindow.xaml.cs
+++ WpfAppKfzKennzeichenApi/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Net.Http;
+﻿
+using System.Diagnostics;
+using System.Net.Http;
+using System.Net.Http.Json;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace WpfAppKfzKennzeichenApi
 {
     /// <summary>
@@ -17,20 +21,86 @@ namespace WpfAppKfzKennzeichenApi
     /// </summary>
     public partial class MainWindow : Window
     {
-        HttpClient client;
+        HttpClient client = new();
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnGetKennzeichen_Click(object sender, RoutedEventArgs e)
+        private async void btnGetKennzeichen_Click(object sender, RoutedEventArgs e)
         {
+            await getData();
+        }
 
+        private async Task getData()
+        {
+            string strResult = await client.GetStringAsync(client.BaseAddress);
+            lbKennzeichen.Items.Add(strResult);
+            Kennzeichen[]? infos = await client.GetFromJsonAsync<Kennzeichen[]>(client.BaseAddress);
+            if (infos == null)
+                return;
+            List<Kennzeichen> listInfos = new List<Kennzeichen>();
+            if (listInfos == null)
+                return;
+            listInfos.AddRange(infos);
+            gridKennzeichen.ItemsSource = listInfos;
         }
 
         private void btnBeenden_Click(object sender, RoutedEventArgs e)
         {
             Close();
+        }
+
+        private async void btnPost_Click(object sender, RoutedEventArgs e)
+        {
+
+            Kennzeichen? k = new Kennzeichen(txtKennzeichen.Text,txtOrt.Text, txtKreis.Text,txtBundesland.Text,txtWappen.Text);
+            if(k == null) return;
+            await client.PostAsJsonAsync<Kennzeichen>(client.BaseAddress,k);
+            await getData();
+        }
+
+        private void gridKennzeichen_SelectedCellsChanged(object sender, SelectedCellsChangedEventArgs e)
+        {
+            if(gridKennzeichen.SelectedItem == null)
+                return;
+            Kennzeichen? k = gridKennzeichen.SelectedItem as Kennzeichen;
+            txtId.Text = k.id.ToString();
+            txtKennzeichen.Text = k.kennzeichen;
+            txtOrt.Text = k.ort;
+            txtKreis.Text =k.kreis;
+            txtBundesland.Text =k.bundesland;
+            txtWappen.Text =k.wappen;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            client.BaseAddress = new Uri("https://localhost:7296/api/KennzeichenInfos/");
+        }
+
+        private async void btnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+            Kennzeichen? k = new Kennzeichen(Convert.ToInt32(txtId.Text), txtKennzeichen.Text, txtOrt.Text, txtKreis.Text, txtBundesland.Text, txtWappen.Text);
+            if(k==null) return;
+            try
+            {
+                //string s = $"https://localhost:7296/api/KennzeichenInfos/{k.id}";
+                //client.BaseAddress= new Uri(s) ;
+                 Task<HttpResponseMessage> t =  client.PutAsJsonAsync<Kennzeichen>(client.BaseAddress, k);
+                await getData();
+            }
+            catch(Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        private async void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            Kennzeichen? k = new Kennzeichen(Convert.ToInt32(txtId.Text), txtKennzeichen.Text, txtOrt.Text, txtKreis.Text, txtBundesland.Text, txtWappen.Text);
+            if (k == null) return;
+            await client.DeleteFromJsonAsync<Kennzeichen>(client.BaseAddress);
+            await getData();
         }
     }
 
